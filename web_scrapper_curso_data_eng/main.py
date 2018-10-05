0d2906ee41087b82196cc4b78098dea6106a8587
David Aroesti <josedavid.aroesti@gmail.com>
2018-10-05 10:10:48 -0500

    Live session

---
+++ web_scrapper_curso_data_eng/main.py
@@ -0,0 +1,28 @@
+import argparse
+import logging
+logging.basicConfig(level=logging.INFO)
+
+from common import config
+
+
+logger = logging.getLogger(__name__)
+
+
+def _news_scraper(news_site_uid):
+    host = config()['news_sites'][news_site_uid]['url']
+
+    logging.info('Beginning scraper for {}'.format(host))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    news_site_choices = list(config()['news_sites'].keys())
+    parser.add_argument('news_site',
+                        help='The news site that you want to scrape',
+                        type=str,
+                        choices=news_site_choices)
+
+    args = parser.parse_args()
+    _news_scraper(args.news_site)
+
