a7d62bd72fa1b9796f39f0b78467fb284df15b87
David Aroesti <josedavid.aroesti@gmail.com>
2018-10-05 09:41:56 -0500

    bs4 demos

--- news_scraper/news_page_objects.py
+++ news_scraper/news_page_objects.py
@@ -26,6 +26,7 @@ class NewsPage:
 class HomePage(NewsPage):
 
     def __init__(self, news_site_uid, url):
+        self._url = url
         super().__init__(news_site_uid, url)
 
     @property
@@ -41,6 +42,7 @@ class HomePage(NewsPage):
 class ArticlePage(NewsPage):
 
     def __init__(self, news_site_uid, url):
+        self._url = url
         super().__init__(news_site_uid, url)
 
     @property
@@ -53,3 +55,6 @@ class ArticlePage(NewsPage):
         result = self._select(self._queries['article_title'])
         return result[0].text if len(result) else ''
 
+    @property
+    def url(self):
+        return self._url
