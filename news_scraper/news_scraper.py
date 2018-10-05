f3e90cc5e47259f9e9b1a32bd84a5abb9e5f585a
David Aroesti <josedavid.aroesti@gmail.com>
2018-10-05 02:34:50 -0500

    Save files to disk

--- news_scraper/news_scraper.py
+++ news_scraper/news_scraper.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python3
 import argparse
+import csv
+import datetime
 import logging
 logging.basicConfig(level=logging.INFO)
 import re
@@ -31,7 +34,7 @@ def _news_scraper(news_site_uid):
             articles.append(article)
             print(article.title)
 
-    print(len(articles))
+    _save_articles(news_site_uid, articles)
 
 
 def _fetch_article(news_site_uid, host, link):
@@ -41,10 +44,10 @@ def _fetch_article(news_site_uid, host, link):
     try:
         article = news.ArticlePage(news_site_uid, _build_link(host, link))
     except (HTTPError, MaxRetryError) as e:
-        logger.warn('Error while fetching article!', exc_info=False)
+        logger.warning('Error while fetching article!', exc_info=False)
 
     if article and not article.body:
-        logger.warn('Invalid article. There is no body.')
+        logger.warning('Invalid article. There is no body.')
         return None
 
     return article
@@ -57,6 +60,20 @@ def _build_link(host, link):
         return '{host}{uri}'.format(host=host, uri=link)
     else:
         return '{host}/{uri}'.format(host=host, uri=link)
+
+
+def _save_articles(news_site_uid, articles):
+    now = datetime.datetime.now()
+    csv_headers = list(filter(lambda property: not property.startswith('_'), dir(articles[0])))
+    out_file_name = '{news_site_uid}_{datetime}_articles.csv'.format(news_site_uid=news_site_uid, datetime=now.strftime('%Y_%m_%d'))
+
+    with open(out_file_name, mode='w+') as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_headers)
+
+        for article in articles:
+            row = [str(getattr(article, prop)) for prop in csv_headers]
+            writer.writerow(row)
 
 
 if __name__ == '__main__':
