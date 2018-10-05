ffe94d7527ebb4877e67930e9dcb6d3dc9f99847
David Aroesti <josedavid.aroesti@gmail.com>
2018-10-05 11:14:59 -0500

    live class part 1

--- web_scrapper_curso_data_eng/news_page_objects.py
+++ web_scrapper_curso_data_eng/news_page_objects.py
@@ -3,8 +3,7 @@ import requests
 
 from common import config
 
-
-class HomePage:
+class NewsPage:
 
     def __init__(self, news_site_uid, url):
         self._config = config()['news_sites'][news_site_uid]
@@ -12,6 +11,22 @@ class HomePage:
         self._html = None
 
         self._visit(url)
+
+    def _select(self, query_string):
+        return self._html.select(query_string)
+
+    def _visit(self, url):
+        response = requests.get(url)
+
+        response.raise_for_status()
+
+        self._html = bs4.BeautifulSoup(response.text, 'html.parser')
+
+
+class HomePage(NewsPage):
+
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url)
 
     @property
     def article_links(self):
@@ -23,13 +38,18 @@ class HomePage:
         return set(link['href'] for link in link_list)
 
 
-    def _select(self, query_string):
-        return self._html.select(query_string)
+class ArticlePage(NewsPage):
 
-    def _visit(self, url):
-        response = requests.get(url)
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url) 
 
-        response.raise_for_status()
+    @property
+    def body(self):
+        result = self._select(self._queries['article_body'])
+        return result[0].text if len(result) else ''
 
-        self._html = bs4.BeautifulSoup(response.text, 'html.parser')
+    @property
+    def title(self):
+        result = self._select(self._queries['article_title'])
+        return result[0].text if len(result) else ''
 
