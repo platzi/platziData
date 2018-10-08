9be11633d6567612057531f1d688800ebc9b24cd
David Aroesti <josedavid.aroesti@gmail.com>
2018-10-08 12:55:47 -0500

    missing data - code

--- web_scrapper_curso_data_eng/newspaper_receipe.py
+++ web_scrapper_curso_data_eng/newspaper_receipe.py
@@ -17,6 +17,7 @@ def main(filename):
     newspaper_uid = _extract_newspaper_uid(filename)
     df = _add_newspaper_uid_column(df, newspaper_uid)
     df = _extract_host(df)
+    df = _fill_missing_titles(df)
 
     return df
 
@@ -45,6 +46,21 @@ def _add_newspaper_uid_column(df, newspaper_uid):
 def _extract_host(df):
     logger.info('Extracting host from urls')
     df['host'] = df['url'].apply(lambda url: urlparse(url).netloc)
+
+    return df
+
+
+def _fill_missing_titles(df):
+    logger.info('Filling missing titles')
+    missing_titles_mask = df['title'].isna()
+
+    missing_titles = (df[missing_titles_mask]['url']
+                        .str.extract(r'(?P<missing_titles>[^/]+)$')
+                        .applymap(lambda title: title.split('-'))
+                        .applymap(lambda title_word_list: ' '.join(title_word_list))
+                      )
+
+    df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:, 'missing_titles']
 
     return df
 
