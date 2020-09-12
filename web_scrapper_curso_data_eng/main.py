9880506ca40a7afffbfd04fe44607208d30cc20a
Miguel Gutierrez <migutie@gmail.com>
2020-09-11 22:23:22 -0500

    Últimas modificaciones al scraper, guarda en archivo csv

--- web_scrapper_curso_data_eng/main.py
+++ web_scrapper_curso_data_eng/main.py
@@ -1,16 +1,19 @@
-# necesita el metodo config() de common.py para cargar la información de config.yaml.
-# >> main.py -h muestra las fuentes de información validas.
-# >> main.py [fuente de información] ejemplo: main.py semana
-# esto se hace con el fin de obtener la url de la fuente.
-
 import argparse
+import csv
+import datetime
 import logging
 logging.basicConfig(level=logging.INFO)
+import re
+
+from requests.exceptions import HTTPError
+from urllib3.exceptions import MaxRetryError
 
 import news_page_objects as news
 from common import config
 
 logger = logging.getLogger(__name__)
+is_well_formed_link = re.compile(r'^https?://.+/.+$') #https://example.com/hello
+is_root_path = re.compile(r'^/.+$') # /algun-texto
 
 def _news_scraper(news_site_uid):
     host = config()['news_sites'][news_site_uid]['url']
@@ -18,13 +21,58 @@ def _news_scraper(news_site_uid):
     logging.info('Iniciando scraper de {}'.format(host))
     homepage = news.HomePage(news_site_uid, host)
 
+    articles = []
     for link in homepage.article_links:
-        print(link)
+        article = _fetch_article(news_site_uid, host, link)
+
+        if article:
+            logger.info('¡Articulo enlazado!')
+            articles.append(article)
+            break #Opcional para tomar solo el primer articulo
+
+    _save_articles(news_site_uid, articles)
+
+def _save_articles(news_site_uid, articles):
+    hoy = datetime.datetime.now().strftime('%Y_%m_%d')
+    out_file_name = '{news_site_uid}_{datetime}_articles.csv'.format(
+        news_site_uid=news_site_uid,
+        datetime=hoy)
+    csv_headers = list(filter(lambda property: not property.startswith('_'), dir(articles[0])))
+
+    with open(out_file_name, mode='w+') as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_headers)
+
+        for article in articles:
+            row = [str(getattr(article,prop)) for prop in csv_headers]
+            writer.writerow(row)
+
+def _fetch_article(news_site_uid, host, link):
+    logger.info('Comenzando a enlazar el articulo {}'.format(link))
+    article = None
+
+    try:
+        article = news.ArticlePage(news_site_uid, _build_link(host, link))
+    except (HTTPError, MaxRetryError) as e:
+        logger.warning('Error enlazando el articulo', exc_info=False)
+    if article and not article.body:
+        logger.warning('Articulo invalido. No hay cuerpo')
+        return None
+    return article
+
+def _build_link(host, link):
+    if is_well_formed_link.match(link):
+        return link
+    elif is_root_path.match(link):
+        return f'{host}{link}'
+    else:
+        return f'{host}/{link}'
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
     news_site_choices = list(config()['news_sites'].keys())
 
-    parser = argparse.ArgumentParser()
     parser.add_argument('news_site',
                         help='Los sitios de noticias que quieres scrapear',
                         type=str,
--- web_scrapper_curso_data_eng/news_page_objects.py
+++ web_scrapper_curso_data_eng/news_page_objects.py
@@ -3,7 +3,7 @@ import requests
 
 from common import config
 
-class HomePage:
+class NewsPage:
 
     def __init__(self, news_site_uid, url):
         self._config = config()['news_sites'][news_site_uid]
@@ -11,6 +11,22 @@ class HomePage:
         self._html = None
         self._url = url
         self._visit(self._url)
+
+    def _select(self, query_string):
+        return self._html.select(query_string)
+
+    def _visit(self, url):
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            self._html = bs4.BeautifulSoup(response.text, 'html.parser')
+        except:
+            self._html = bs4.BeautifulSoup('')
+
+class HomePage(NewsPage):
+
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url)
 
     @property
     def article_links(self):
@@ -21,47 +37,21 @@ class HomePage:
 
         return set(link['href'] for link in link_list)
 
-    def _select(self, query_string):
-        return self._html.select(query_string)
+class ArticlePage(NewsPage):
 
-    def _visit(self, url):
-        response = requests.get(url)
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url) 
 
-        response.raise_for_status()
+    @property
+    def body(self):
+        result = self._select(self._queries['article_body'])
+        return result[0].text if len(result) else ''
 
-        self._html = bs4.BeautifulSoup(response.text, 'html.parser')
+    @property
+    def title(self):
+        result = self._select(self._queries['article_title'])
+        return result[0].text if len(result) else ''
 
-# class HomePage(NewsPage):
-
-#     def __init__(self, news_site_uid, url):
-#         super().__init__(news_site_uid, url)
-
-#     @property
-#     def article_links(self):
-#         link_list = []
-#         for link in self._select(self._queries['homepage_article_links']):
-#             if link and link.has_attr('href'):
-#                 link_list.append(link)
-
-#         return set(link['href'] for link in link_list)
-
-
-# class ArticlePage(NewsPage):
-
-#     def __init__(self, news_site_uid, url):
-#         super().__init__(news_site_uid, url) 
-
-#     @property
-#     def body(self):
-#         result = self._select(self._queries['article_body'])
-#         return result[0].text if len(result) else ''
-
-#     @property
-#     def title(self):
-#         result = self._select(self._queries['article_title'])
-#         return result[0].text if len(result) else ''
-
-#     @property
-#     def url(self):
-#         return self._url
-
+    @property
+    def url(self):
+        return self._url
